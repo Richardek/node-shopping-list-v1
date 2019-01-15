67c6b1978a1618782c18763ed3a1f348868b8752
Richard McBride <richgametv@gmail.com>
2019-01-14 22:37:31 -0500

    change server file

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.get('/shopping-list', (req, res) => {
 });
 
 app.get('/recipes', (req, res) => {
-  res.json(ShoppingList.get());
+  res.json(Recipes.get());
 });
 
 app.listen(process.env.PORT || 8080, () => {
